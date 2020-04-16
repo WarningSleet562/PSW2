6e301bc474da5fba544f4f5ce5dfea132ea7f2f5
brunoluizs <y.bruno@live.com>
2020-04-16 06:37:36 -0300

    Revert "Revert "Adicionado métodos para Salvar e Abrir arquivos.""
    This reverts commit 1f95bd487d22040794d00f6c6a71df4a220bcea6.

--- Aplicativo_Desenhos/src/br/edu/udc/gui/PainelDesenho.java
+++ Aplicativo_Desenhos/src/br/edu/udc/gui/PainelDesenho.java
@@ -5,6 +5,13 @@ import java.util.List;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.LinkedList;
 
 import javax.swing.JLabel;
@@ -117,5 +124,45 @@ public class PainelDesenho extends JPanel implements MouseListener, MouseMotionL
 		status.setText(message);		
 	}
 
+	public void salvar(File file) {
+		try {
+			ObjectOutputStream oss = new ObjectOutputStream(new FileOutputStream(file));
+			
+			for (FormaGeometrica f : listaFormas) {
+				oss.writeObject(f);
+			}
+			
+			oss.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void ler(File file) {
+		ObjectInputStream ois = null;
+		
+		try {
+			ois = new ObjectInputStream(new FileInputStream(file));
+			
+			listaFormas.clear();
+			
+			FormaGeometrica formaAux = null;
+			while (true) {
+				formaAux = (FormaGeometrica) ois.readObject();
+				listaFormas.add(formaAux);
+			}
+		} catch (EOFException endOfFileException) {
+			try {
+				ois.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		repaint();
+	}
 	
 }
